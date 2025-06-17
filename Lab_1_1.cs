2e2690e37b3391496155bb9c032b7e53969b2563
Bhavy123321 <140868488+Bhavy123321@users.noreply.github.com>
2025-06-17 21:27:06 +0530

    Add files via upload

---
+++ Lab_1_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Sem_5
+{
+    public class Lab_1_1
+    {
+        public void demo()
+        {
+            Console.WriteLine("Enter Your Name Here");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Address Here");
+            string Address = Console.ReadLine();
+
+            Console.WriteLine("Enter Your City Here");
+            string City = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Contact Here");
+            string Contact = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Your Personal Information");
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Address : " + Address);
+            Console.WriteLine("City : " + City);
+            Console.WriteLine("Contact : " + Contact);
+        }
+    }
+}
